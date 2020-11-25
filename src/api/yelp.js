d6396378bbc66b3c17c7b3945071283264bf76f5
Casey Spain <spaincasey7@gmail.com>
2020-11-24 17:48:12 -0700

    got rid of api token

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,12 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer a-tASZi5oKrEajSzl6KImjewQx1FkKyXor9f6vEutIrEBjAHLtnfNbsEVBM5qIQzqbUwOW-b4STUYhl8y3z_Mla6IGK9MGtXU60LLDPw5mGag6nqu2IElgTYyRu3X3Yx'
+        Authorization: 'Bearer '
     }
 });
-
-// Client ID
-// 52jsXfPeCQXRpI1LI6UgrQ
-
-// API Key
-// a-tASZi5oKrEajSzl6KImjewQx1FkKyXor9f6vEutIrEBjAHLtnfNbsEVBM5qIQzqbUwOW-b4STUYhl8y3z_Mla6IGK9MGtXU60LLDPw5mGag6nqu2IElgTYyRu3X3Yx
